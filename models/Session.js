6609092f6b3398921b5621a1323e8f8418e4e2c7
Prajna1999 <32980302+Prajna1999@users.noreply.github.com>
2023-04-29 16:12:35 +0000

    fix bug

--- models/Session.js
+++ models/Session.js
@@ -5,6 +5,7 @@ class Session {
     // this.user = new User(username, password);
     this.user = new User(username, password);
     this.isLoggedIn = false;
+    this.newsfeed=[];
   }
 
   login() {
@@ -46,7 +47,59 @@ class Session {
       console.error('User is not logged in.');
     }
   }
+  // postItem to the wall
+  postItem(content){
+    if(this.isLoggedIn){
+      const post=new Post(content, this.user);
+      this.newsfeed.push(post);
+      console.log(`User ${this.user.username} posted: ${content}`)
+    }else{
+      console.error('User not logged in');
+      
+    }
+  }
 
+  // upvote post
+  upvotePost(post){
+    if(this.isLoggedIn){
+      post.upvote();
+      console.log(`User ${this.user.username} upvoted the post with ID ${post.id}`);
+    }else{
+      console.error("User not logged in");
+    }
+  }
+
+  // downvote post
+  downvotePost(post){
+    if(this.isLoggedIn){
+      post.downvote();
+      console.log(`User ${this.user.username} downvoted the post with ID ${post.id}`);
+    }else{
+      console.error("User not logged in");
+    }
+  }
+
+  //get news feed sorted by some properties
+  // we are using default properties 
+  
+  getNewsFeed(sortBy="mostRecent") {
+    if(this.isLoggedIn){
+      const sortedFeed=this.newsfeed.sort((a,b)=>{
+        if(sortBy==="mostRecent"){
+          return b.timestamp-a.timestamp;
+        }else if(sortBy==="mostUpvoted"){
+          return b.upvotes.length-a.upvotes.length;
+        }else if(sortBy==="mostCommented"){
+           return b.comments.length-a.comments.length;   
+        }
+      });
+      console.log(`News feed for user ${this.user.username} sorted by ${sortBy}`)
+      sortedFeed.forEach(post=>console.log(post))
+    }else{
+      console.error("User not logged in");
+    }
+  }
+  
   static signup(username, password) {
     const newUser = new User(username, password);
     newUser.signup();
